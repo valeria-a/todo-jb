d55d6e6e84055189119fdcbaeba38559264085d5
Valeria Aynbinder <valeria.aynbinder@gmail.com>
2023-07-03 18:36:06 +0300

    sfsdfsd

---
+++ src/AddTaskForm/AddTaskForm.jsx
@@ -0,0 +1,12 @@
+import { TextField } from "@mui/material"
+
+const AddTaskForm = () => {
+    return(
+        <>
+        <p>AddTaskForm</p>
+            <TextField id="standard-basic" label="Standard" variant="standard" />
+        </>
+    )
+
+}
+export default AddTaskForm
--- src/App.js
+++ src/App.js
@@ -1,9 +1,17 @@
+import { useState } from 'react';
 import './App.css';
 import Header from './Header/Header';
+import AddTaskForm from './AddTaskForm/AddTaskForm';
 
 function App() {
+
+  const [tasks, setTasks] = useState([])
+
   return (
-    <Header />
+    <>
+      <Header />
+      <AddTaskForm />
+    </>
   );
 }
 
